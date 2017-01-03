c5b037366310a10b20b4284a1c7b27a9d8132bcc
Nikolai Bock <nikolai.bock@hs-mainz.de>
2017-01-03 14:32:41 +0100

    matcher in configure method

--- src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
+++ src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
@@ -17,24 +17,25 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	// Build the request matcher for CSFR
-	RequestMatcher csrfRequestMatcher = new RequestMatcher() {
-
-		private RegexRequestMatcher requestMatcher = new RegexRequestMatcher("/events/**", null);
-
-		@Override
-		public boolean matches(HttpServletRequest request) {
-
-			if (requestMatcher.matches(request))
-				return false;
-
-			return true;
-		}
-
-	};
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		// Build the request matcher for CSFR
+		RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+
+			private RegexRequestMatcher requestMatcher = new RegexRequestMatcher("/events/**", null);
+
+			@Override
+			public boolean matches(HttpServletRequest request) {
+
+				if (requestMatcher.matches(request))
+					return false;
+
+				return true;
+			}
+
+		};
+		
 		http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and().authorizeRequests()
 				.antMatchers("/css/**", "/js/**", "/img/**", "/elements/**", "/webjars/**").permitAll()
 				.antMatchers("/", "/map", "/find", "/AddFind", "/layer", "/ballooncodeerror", "/ballooncodeexists",
