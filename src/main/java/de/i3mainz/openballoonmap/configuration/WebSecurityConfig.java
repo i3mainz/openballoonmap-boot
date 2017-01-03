771f6ff0718cee0f66072cce9d02e06fa53672e3
Nikolai Bock <nikolai.bock@hs-mainz.de>
2017-01-03 14:44:56 +0100

    AntPathRequestMatcher for disabled URL

--- src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
+++ src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
@@ -19,23 +19,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		// Build the request matcher for CSFR
 		RequestMatcher csrfRequestMatcher = new RequestMatcher() {
 
-			private RegexRequestMatcher requestMatcher = new RegexRequestMatcher("/events/**", null);
+			// Disable CSFR protection on the following urls:
+			private AntPathRequestMatcher[] requestMatchers = { new AntPathRequestMatcher("/events/**") };
 
 			@Override
 			public boolean matches(HttpServletRequest request) {
 
-				if (requestMatcher.matches(request))
-					return false;
+				for (AntPathRequestMatcher rm : requestMatchers) {
+					if (rm.matches(request)) {
+						return false;
+					}
+				}
 
 				return true;
 			}
 
 		};
-		
+
 		http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and().authorizeRequests()
 				.antMatchers("/css/**", "/js/**", "/img/**", "/elements/**", "/webjars/**").permitAll()
 				.antMatchers("/", "/map", "/find", "/AddFind", "/layer", "/ballooncodeerror", "/ballooncodeexists",
