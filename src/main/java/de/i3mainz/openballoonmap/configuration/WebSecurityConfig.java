fcc641c7ec0e9a6a96d5b5f838a23d2483ce0c5b
Nikolai Bock <nikolai.bock@hs-mainz.de>
2017-01-03 14:24:15 +0100

    disable CSRF on event deletion

--- src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
+++ src/main/java/de/i3mainz/openballoonmap/configuration/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package de.i3mainz.openballoonmap.configuration;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * @author Nikolai Bock
@@ -12,19 +16,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            	.antMatchers("/css/**", "/js/**", "/img/**", "/elements/**", "/webjars/**").permitAll()
-                .antMatchers("/", "/map", "/find", "/AddFind", "/layer", "/ballooncodeerror", "/ballooncodeexists", "/thx").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+
+	// Build the request matcher for CSFR
+	RequestMatcher csrfRequestMatcher = new RequestMatcher() {
+
+		private RegexRequestMatcher requestMatcher = new RegexRequestMatcher("/events/**", null);
+
+		@Override
+		public boolean matches(HttpServletRequest request) {
+
+			if (requestMatcher.matches(request))
+				return false;
+
+			return true;
+		}
+
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher).and().authorizeRequests()
+				.antMatchers("/css/**", "/js/**", "/img/**", "/elements/**", "/webjars/**").permitAll()
+				.antMatchers("/", "/map", "/find", "/AddFind", "/layer", "/ballooncodeerror", "/ballooncodeexists",
+						"/thx")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.logout().permitAll();
+	}
 }
