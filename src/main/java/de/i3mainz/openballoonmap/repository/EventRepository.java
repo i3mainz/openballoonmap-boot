f8324ad10fd7255b4d7a3fdfea65f105ee9e531a
Nikolai Bock <nikolai.bock@hs-mainz.de>
2017-01-03 15:56:10 +0100

    delete transactional

--- src/main/java/de/i3mainz/openballoonmap/repository/EventRepository.java
+++ src/main/java/de/i3mainz/openballoonmap/repository/EventRepository.java
@@ -3,6 +3,8 @@
  */
 package de.i3mainz.openballoonmap.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import de.i3mainz.openballoonmap.model.Event;
@@ -14,6 +16,8 @@ import de.i3mainz.openballoonmap.model.Event;
 public interface EventRepository extends CrudRepository<Event, Long> {
 
 	public Event findByName(String name);
+
+	@Transactional
 	public void deleteByName(String name);
 
 }
